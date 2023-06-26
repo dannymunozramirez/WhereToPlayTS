0bed03a214e826d054dbe7e92df36fa991d76618
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-06-26 11:56:42 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Hi!
+      </Text>
+    </SafeAreaView>
+  )
+}
