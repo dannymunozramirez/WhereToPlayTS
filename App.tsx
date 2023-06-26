fb5e86641332533f35fa2e010ef53969c4bb94f5
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-06-26 12:01:28 -0400

    Test Added components - pass

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { CompTest } from './src/components/CompTest'
+import { Test } from './src/screens/Test'
 
 export const App = () => {
   return (
     <SafeAreaView>
       <Text>
-        Hi!
+        <Test />
+        <CompTest />
       </Text>
     </SafeAreaView>
   )
---
+++ src/screens/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Test = () => {
+  return (
+    <View>
+        <Text>
+            Hi Test
+        </Text>
+    </View>
+  )
+}
