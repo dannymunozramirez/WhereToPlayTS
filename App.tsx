ade68bc33bb5d21662940ae61a7fd77290348507
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-07-03 17:07:12 -0400

    project working well - added safe area context

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { Tabs } from './src/components/Tabs';
 import { Navigator } from './src/navigator/Navigator';
 
 export const App = () => {
