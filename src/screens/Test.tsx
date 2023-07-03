541a3e23bf9812fdec2bc9615204535cc4b2e047
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-07-03 10:36:56 -0400

    added Icons and bottom tabs

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -1,6 +1,8 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Navigator } from '../navigator/Navigator'
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 
 interface Props extends StackScreenProps<any, any> { }
 
@@ -8,6 +10,7 @@ export const Test = ({ navigation }: Props) => {
 
     return (
         <View>
+            
             <TouchableOpacity style={styles.touchableStyle} activeOpacity={0.8}
                 // This onPress attribute will address to LoginForm using Stack.Navigator
                 onPress={() => navigation.replace('TestII')}
