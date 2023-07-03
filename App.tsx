2d9562d755184180a521708a3c35130b1801b3b9
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-07-03 10:37:35 -0400

    added icons and bottom tab

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
+import { Tabs } from './src/components/Tabs';
 import { Navigator } from './src/navigator/Navigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
 
-      <Navigator />
+      {/* <Navigator /> */}
+      <Tabs />
 
     </NavigationContainer>
 
